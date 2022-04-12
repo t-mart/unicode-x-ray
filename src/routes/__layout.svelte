3f3e1d638322183dd9df435871ecd0fd6ea7b384
Tim Martin <tim@timmart.in>
2022-04-12 15:05:46 -0500

    Add more top margin to footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,7 @@
 <div class="grid grid-rows-[repeat(3,_min-content)_1fr] gap-4 py-4 px-4 mx-0 min-h-screen lg:mx-auto lg:container">
   <h1 class="text-4xl">Unicode X-Ray</h1>
   <slot />
-  <footer class="text-stone-500 self-end justify-self-center">
+  <footer class="text-stone-500 self-end justify-self-center mt-8">
     Tim Martin, <a href="https://github.com/t-mart/unicode-x-ray" class="link">source</a>
   </footer>
 </div>
