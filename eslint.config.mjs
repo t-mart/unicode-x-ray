3fcd94a744c14429a3cab5aff5b6a8a168d8ec1f
Tim Martin <tim@timmart.in>
2025-02-19 22:15:16 -0600

    ignore next build dir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { ignores: ['.next'] },
 ];
 
 export default eslintConfig;
